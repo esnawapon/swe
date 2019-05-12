f3739ca9db949efcd8c967a7d7f34b006976e27e
peeranut <peekung.konoha@gmail.com>
2019-05-12 16:46:29 +0700

    fix mapping resources

---
+++ bac-complaint/src/main/java/com/bookacourse/complaint/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.bookacourse.complaint.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+  @Value("${spring.resources.static-locations}")
+  String resourceLocations;
+  
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/public/**").addResourceLocations(resourceLocations);
+  }
+}
